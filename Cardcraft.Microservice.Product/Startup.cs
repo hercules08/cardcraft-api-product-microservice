4c02bdc71cab94458a7b5203f27eba93bf2db385
Damola Omotosho <damola.omotosho@gmail.com>
2019-03-13 17:31:42 -0700

    Support static files

--- Cardcraft.Microservice.Product/Startup.cs
+++ Cardcraft.Microservice.Product/Startup.cs
@@ -75,6 +75,8 @@ namespace Cardcraft.Microservice.Product
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
             });
+
+            app.UseStaticFiles();
             app.UseMvc();
         }
     }
